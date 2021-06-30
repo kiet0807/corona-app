6e271f61013a1acebd32d53e3697068ee17a929a
kiet0807 <kiet080797@gmail.com>
2021-06-30 21:42:27 +0700

    fix

--- stories/CardVideoSymptoms.stories.tsx
+++ stories/CardVideoSymptoms.stories.tsx
@@ -1,19 +1,11 @@
-import React from 'react';
-import { withKnobs} from '@storybook/addon-knobs';
-import CardVideoSymptoms from '../components/CardVideoSymptoms'
-import {Box} from 'theme-ui'
+import React from "react";
+import { withKnobs } from "@storybook/addon-knobs";
+import CardVideoSymptoms from "../components/CardVideoSymptoms";
+import { Box } from "theme-ui";
 
 export default {
-  title: 'Card',
+  title: "Card",
   decorators: [withKnobs],
 };
 
-export const WithACardVideoSymptoms = () => (
-  <Box bg={'grey'} p = {20}>
-    <CardVideoSymptoms
-        video = "https://www.youtube.com/embed/bSnBrVmFoaQ"
-        name = 'What Coronavirus Symptoms Look Like'
-        description = 'After being exposed to the virus that cause COVID-19, it can take as 2 to 4 days to develop.'
-    />
-  </Box>
-);
+export const WithACardVideoSymptoms = () => <Box bg={"grey"} p={20}></Box>;
