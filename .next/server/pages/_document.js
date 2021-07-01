02ee00e34023088be96f26deda28809268e4ae69
kiet0807 <kiet080797@gmail.com>
2021-07-01 17:19:52 +0700

    fix

--- .next/server/pages/_document.js
+++ .next/server/pages/_document.js
@@ -88,7 +88,7 @@ module.exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 5);
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -355,7 +355,7 @@ exports.default = _default;
 
 /***/ }),
 
-/***/ 5:
+/***/ 4:
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__("VDXt");
